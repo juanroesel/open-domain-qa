a40ff63152043c39c85b017b6408c6572b958399
juanroesel <juan@helelab.org>
2022-05-23 14:07:04 -0700

    API call test working OK

--- odqabert/bert_model/views.py
+++ odqabert/bert_model/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .apps import BertModelConfig
 
-# Create your views here.
+
+class call_model(APIView):
+    """Testing view to test API requests calls to the QA BERT model."""
+    def get(self, request):
+        if request.method == 'GET':
+            # sentence is the query we want to get the prediction for
+            question = request.GET.get('question') + "?"
+            context = "This is a dummy context to help test the API call"
+            # predict method used to get the prediction
+            print(question)
+            response = BertModelConfig.QA_BERT_model(
+                    question=question,
+                    context=context
+                )
+            # returning JSON response
+            print(response)
+            return JsonResponse(response, safe=False)
